59ee390974538d615a414d08d8bec5770cf82ae0
MarcosCesar <marcoscesarcz@gmail.com>
2025-11-04 14:41:46 -0300

    desenvolvimento do CSS e HTML junto da logica de abertura e fechamento do modal de excluir

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal-excluir');
+    const botaoAbrir = document.getElementById('abrir-modal-btn');
+    const botaoSim = document.getElementById('btn-sim');
+    const botaoNao = document.getElementById('btn-nao');
+
+    // abrir e fechar
+    const abrirModal = () => {
+        modal.classList.remove('hidden'); // remove a classe "hidden"
+    };
+
+    const fecharModal = () => {
+        modal.classList.add('hidden'); // adiciona a classe "hidden"
+    };
+
+    botaoAbrir.addEventListener('click', abrirModal);
+    botaoSim.addEventListener('click', fecharModal);
+    botaoNao.addEventListener('click', fecharModal);
