3455e906e532371fe63216d5ebb7ffc7b443ec19
MarcosCesar <marcoscesarcz@gmail.com>
2025-11-05 13:14:00 -0300

    Muddando aluns nomes

--- public/js/ModalExcluirPost.js
+++ public/js/ModalExcluirPost.js
@@ -1,17 +1,33 @@
-const modal = document.getElementById('modal-excluir');
-    const botaoAbrir = document.getElementById('abrir-modal-btn');
+const modal = document.getElementById('modal-excluir-post');
+
+    const botaoAbrir = document.getElementById('lixeira-post');
+
     const botaoSim = document.getElementById('btn-sim');
+
     const botaoNao = document.getElementById('btn-nao');
 
+
+
     // abrir e fechar
+
     const abrirModal = () => {
+
         modal.classList.remove('hidden'); // remove a classe "hidden"
+
     };
+
+
 
     const fecharModal = () => {
+
         modal.classList.add('hidden'); // adiciona a classe "hidden"
+
     };
 
+
+
     botaoAbrir.addEventListener('click', abrirModal);
+
     botaoSim.addEventListener('click', fecharModal);
+
     botaoNao.addEventListener('click', fecharModal);
---
+++ public/js/ModalExcluirUsuario.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById('modal-excluir-usuario');
+
+    const botaoAbrir = document.getElementById('lixeira-usuario');
+
+    const botaoSim = document.getElementById('btn-sim');
+
+    const botaoNao = document.getElementById('btn-nao');
+
+
+
+    // abrir e fechar
+
+    const abrirModal = () => {
+
+        modal.classList.remove('hidden'); // remove a classe "hidden"
+
+    };
+
+
+
+    const fecharModal = () => {
+
+        modal.classList.add('hidden'); // adiciona a classe "hidden"
+
+    };
+
+
+
+    botaoAbrir.addEventListener('click', abrirModal);
+
+    botaoSim.addEventListener('click', fecharModal);
+
+    botaoNao.addEventListener('click', fecharModal);
