eb91873aec8e08e6592d812bf25b4be81db2c821
Bruno <brunoar780@gmail.com>
2025-11-12 00:01:30 -0300

    modais adicionados, mas precisam de alguns ajustes

--- public/js/Modal.js
+++ public/js/Modal.js
@@ -4,12 +4,12 @@ const tela = document.querySelector('#tela');
 function abrirModal(idModal)
 {
 	document.getElementById(idModal).style.display = "flex";
-
+	document.body.style.overflow = "hidden";
 }
 
 function fecharModal(idModal)
 {
 
 	document.getElementById(idModal).style.display = "none";
-
+	document.body.style.overflow = "auto";
 }
