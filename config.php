fab5b1f003770e74716f4092f7e0d90aaebc2c17
Mickaell-Oliveira <mickaell.oliveira@estudante.ufjf.br>
2025-11-13 20:59:46 -0300

    ⚙️ Migra Repo Para Backend

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'rastros_de_pista_db',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
