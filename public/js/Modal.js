8671b61ad68f0b017d46020ccf81906b10531637
MarcosCesar <marcoscesarcz@gmail.com>
2025-11-19 00:03:05 -0300

    fazendo o olho funcionar

--- public/js/Modal.js
+++ public/js/Modal.js
@@ -13,3 +13,19 @@ function fecharModal(idModal)
 	document.getElementById(idModal).style.display = "none";
 	document.body.style.overflow = "auto";
 }
+
+//olho  do modal
+document.addEventListener('DOMContentLoaded', function() {
+    const olhos = document.querySelectorAll('.toggle-password');
+    olhos.forEach(olho => {
+        olho.addEventListener('click', function() {
+            const inputSenha = this.previousElementSibling;
+            if (inputSenha) {
+                const type = inputSenha.getAttribute('type') === 'password' ? 'text' : 'password';
+                inputSenha.setAttribute('type', type);
+                this.classList.toggle('fa-eye-slash'); 
+                this.classList.toggle('fa-eye'); 
+            }
+        });
+    });
+});
